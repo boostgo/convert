3cd0fa214a93011b839c7e223a97821434fce389
Yerik Ussayev <yerik.ussayev@tele2.kz>
2025-03-27 14:52:48 +0500

    feat(float): add Float(x any) float32/64 funcs

---
+++ float.go
@@ -0,0 +1,215 @@
+package convert
+
+func Float32FromInt[T Integer](x T) float32 {
+	return float32(x)
+}
+
+func Float64FromInt[T Integer](x T) float64 {
+	return float64(x)
+}
+
+func Float32(x any) float32 {
+	switch v := x.(type) {
+	case float32:
+		return v
+	case *float32:
+		if v == nil {
+			return 0
+		}
+
+		return *v
+	case float64:
+		return float32(v)
+	case *float64:
+		if v == nil {
+			return 0
+		}
+
+		return float32(*v)
+	case int:
+		return float32(v)
+	case *int:
+		if v == nil {
+			return 0
+		}
+
+		return float32(*v)
+	case int8:
+		return float32(v)
+	case *int8:
+		if v == nil {
+			return 0
+		}
+
+		return float32(*v)
+	case int16:
+		return float32(v)
+	case *int16:
+		if v == nil {
+			return 0
+		}
+
+		return float32(*v)
+	case int32:
+		return float32(v)
+	case *int32:
+		if v == nil {
+			return 0
+		}
+
+		return float32(*v)
+	case int64:
+		return float32(v)
+	case *int64:
+		if v == nil {
+			return 0
+		}
+
+		return float32(*v)
+	case uint:
+		return float32(v)
+	case *uint:
+		if v == nil {
+			return 0
+		}
+
+		return float32(*v)
+	case uint8:
+		return float32(v)
+	case *uint8:
+		if v == nil {
+			return 0
+		}
+
+		return float32(*v)
+	case uint16:
+		return float32(v)
+	case *uint16:
+		if v == nil {
+			return 0
+		}
+
+		return float32(*v)
+	case uint32:
+		return float32(v)
+	case *uint32:
+		if v == nil {
+			return 0
+		}
+
+		return float32(*v)
+	case uint64:
+		return float32(v)
+	case *uint64:
+		if v == nil {
+			return 0
+		}
+
+		return float32(*v)
+	default:
+		return 0
+	}
+}
+
+func Float64(x any) float64 {
+	switch v := x.(type) {
+	case float32:
+		return float64(v)
+	case *float32:
+		if v == nil {
+			return 0
+		}
+
+		return float64(*v)
+	case float64:
+		return v
+	case *float64:
+		if v == nil {
+			return 0
+		}
+
+		return *v
+	case int:
+		return float64(v)
+	case *int:
+		if v == nil {
+			return 0
+		}
+
+		return float64(*v)
+	case int8:
+		return float64(v)
+	case *int8:
+		if v == nil {
+			return 0
+		}
+
+		return float64(*v)
+	case int16:
+		return float64(v)
+	case *int16:
+		if v == nil {
+			return 0
+		}
+
+		return float64(*v)
+	case int32:
+		return float64(v)
+	case *int32:
+		if v == nil {
+			return 0
+		}
+
+		return float64(*v)
+	case int64:
+		return float64(v)
+	case *int64:
+		if v == nil {
+			return 0
+		}
+
+		return float64(*v)
+	case uint:
+		return float64(v)
+	case *uint:
+		if v == nil {
+			return 0
+		}
+
+		return float64(*v)
+	case uint8:
+		return float64(v)
+	case *uint8:
+		if v == nil {
+			return 0
+		}
+
+		return float64(*v)
+	case uint16:
+		return float64(v)
+	case *uint16:
+		if v == nil {
+			return 0
+		}
+
+		return float64(*v)
+	case uint32:
+		return float64(v)
+	case *uint32:
+		if v == nil {
+			return 0
+		}
+
+		return float64(*v)
+	case uint64:
+		return float64(v)
+	case *uint64:
+		if v == nil {
+			return 0
+		}
+
+		return float64(*v)
+	default:
+		return 0
+	}
+}
