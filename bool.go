262d3224068151bf69ed29b807ca9955b01c8867
Yerik Ussayev <yerik.ussayev@tele2.kz>
2025-03-27 14:43:10 +0500

    feat(bool): add Bool(x any) bool func

--- bool.go
+++ bool.go
@@ -9,3 +9,106 @@ func BoolFromString(x string) bool {
 func BoolFromInt[T Integer](x T) bool {
 	return x == 1
 }
+
+func Bool(x any) bool {
+	if x == nil {
+		return false
+	}
+
+	switch v := x.(type) {
+	case bool:
+		return v
+	case *bool:
+		return *v
+	case string:
+		return BoolFromString(v)
+	case *string:
+		if v == nil {
+			return false
+		}
+
+		return BoolFromString(*v)
+	case int:
+		return BoolFromInt(v)
+	case *int:
+		if v == nil {
+			return false
+		}
+
+		return BoolFromInt(*v)
+	case int8:
+		return BoolFromInt(v)
+	case *int8:
+		if v == nil {
+			return false
+		}
+
+		return BoolFromInt(*v)
+	case int16:
+		return BoolFromInt(v)
+	case *int16:
+		if v == nil {
+			return false
+		}
+
+		return BoolFromInt(*v)
+	case int32:
+		return BoolFromInt(v)
+	case *int32:
+		if v == nil {
+			return false
+		}
+
+		return BoolFromInt(*v)
+	case int64:
+		return BoolFromInt(v)
+	case *int64:
+		if v == nil {
+			return false
+		}
+
+		return BoolFromInt(*v)
+	case uint:
+		return BoolFromInt(v)
+	case *uint:
+		if v == nil {
+			return false
+		}
+
+		return BoolFromInt(*v)
+	case uint8:
+		return BoolFromInt(v)
+	case *uint8:
+		if v == nil {
+			return false
+		}
+
+		return BoolFromInt(*v)
+	case uint16:
+		return BoolFromInt(v)
+	case *uint16:
+		if v == nil {
+			return false
+		}
+
+		return BoolFromInt(*v)
+	case uint32:
+		return BoolFromInt(v)
+	case *uint32:
+		if v == nil {
+			return false
+		}
+
+		return BoolFromInt(*v)
+	case uint64:
+		return BoolFromInt(v)
+	case *uint64:
+		if v == nil {
+			return false
+		}
+
+		return BoolFromInt(*v)
+	default:
+		return false
+	}
+}
