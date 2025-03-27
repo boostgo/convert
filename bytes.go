28b7072ddc3d69f41f1f99b835004cf610b12b8a
Yerik Ussayev <yerik.ussayev@tele2.kz>
2025-03-27 14:48:02 +0500

    feat(bytes): add Bytes(x any) []byte func

--- bytes.go
+++ bytes.go
@@ -1,6 +1,7 @@
 package convert
 
 import (
+	"encoding/json"
 	"reflect"
 	"unsafe"
 )
@@ -12,4 +13,30 @@ func BytesFromString(x string) []byte {
 		Len:  len(x),
 		Cap:  len(x),
 	}))
+}
+
+func Bytes(x any) []byte {
+	if x == nil {
+		return nil
+	}
+
+	switch v := x.(type) {
+	case []byte:
+		return v
+	case string:
+		return BytesFromString(v)
+	case *string:
+		if v == nil {
+			return nil
+		}
+
+		return BytesFromString(*v)
+	default:
+		blob, err := json.Marshal(x)
+		if err != nil {
+			return nil
+		}
+
+		return blob
+	}
 }
